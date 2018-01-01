1305924dea9aadd116ad196efc30c71bbcc8d990
gentom <gento.m.developer@gmail.com>
2018-01-02 03:04:09 +0900

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :elxphxchat, ElxphxchatWeb.Endpoint,
   load_from_system_env: true,
   # url: [host: "example.com", port: 80],
-  url: [scheme: "https", host: "afternoon-brook-53413.herokuapp.com", port: 443], 
+  url: [scheme: "https", host: "boiling-savannah-70827.herokuapp.com", port: 443], 
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
